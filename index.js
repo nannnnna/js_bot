a01c895258c33cb451496ee5b47d4e365e54eac6
nannnnna <141238876+nannnnna@users.noreply.github.com>
2023-12-05 18:28:10 +0300

    example

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf('6323438195:AAGpAbUPZJSxsOBnTRvzP-dOyKTH90Q6o-o');
 
-bot.use(async (ctx) => {
-    await ctx.reply(JSON.stringify(ctx.update, null, 2));
+bot.command('help', (ctx) => {
+    ctx.reply(`
+    Бот может здороваться на разных языках.
+    Список поддерживаемых приветствий:
+    - привет - русский
+    - hello - английский
+    - hola - испанский
+    `)
 });
+
+bot.hears('привет', (ctx) => ctx.reply('привет'));
+bot.hears('hello', (ctx) => ctx.reply('hello'));
+bot.hears('hola', (ctx) => ctx.reply('hola'));
+
+bot.on('text', (ctx) => ctx.reply(`Приветствие "${ctx.update.message.text}" не поддерживается.`))
 
 bot.launch().then(() => console.log('Started'));
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
