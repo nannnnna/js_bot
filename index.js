5c083b0a8d59a8cea1f7187907bc4fa6b4ab6668
nannnnna <141238876+nannnnna@users.noreply.github.com>
2023-12-05 18:31:03 +0300

    randomizer

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
-const { Telegraf } = require('telegraf');
+const { Telegraf, Markup } = require('telegraf');
 
 const bot = new Telegraf('6323438195:AAGpAbUPZJSxsOBnTRvzP-dOyKTH90Q6o-o');
 
-bot.command('help', (ctx) => {
-    ctx.reply(`
-    Бот может здороваться на разных языках.
-    Список поддерживаемых приветствий:
-    - привет - русский
-    - hello - английский
-    - hola - испанский
-    `)
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const getCoinSide = () => getRandomInt(0, 1) === 0 ? 'Орёл' : 'Решка';
+const coinInlineKeyboard = Markup.inlineKeyboard([
+    Markup.button.callback('Подбросить ещё раз', 'flip_a_coin'),
+]);
+bot.hears('Подбросить монетку', ctx => ctx.reply(getCoinSide(), coinInlineKeyboard));
+bot.action('flip_a_coin', async(ctx) => {
+    await ctx.editMessageText(`${getCoinSide()}\nОтредактировано: ${new Date().toISOString()}`, coinInlineKeyboard);
 });
 
-bot.hears('привет', (ctx) => ctx.reply('привет'));
-bot.hears('hello', (ctx) => ctx.reply('hello'));
-bot.hears('hola', (ctx) => ctx.reply('hola'));
+const getRandomNumber = () => getRandomInt(0, 100);
+const numberInlineKeyboard = Markup.inlineKeyboard([
+    Markup.button.callback('Сгенерировать новое', 'random_number'),
+]);
+bot.hears('Случайное число', ctx => ctx.reply(getRandomNumber().toString(), numberInlineKeyboard));
+bot.action('random_number', async(ctx) => {
+    await ctx.editMessageText(`${getRandomNumber()}\nОтредактировано: ${new Date().toISOString()}`, numberInlineKeyboard);
+});
 
-bot.on('text', (ctx) => ctx.reply(`Приветствие "${ctx.update.message.text}" не поддерживается.`))
+bot.use(async (ctx) => {
+    await ctx.reply('Что нужно сделать?', Markup
+        .keyboard([
+            ['Подбросить монетку', 'Случайное число'],
+        ]).resize()
+    )
+});
 
 bot.launch().then(() => console.log('Started'));
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
